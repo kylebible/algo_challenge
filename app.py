dfc8b775f7aec0a94ba94d16088b7ae824b52491
Kyle Bible <kyle.bible@gmail.com>
2017-10-30 16:48:29 -0700

    added favicon

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, request, redirect, send_from_directory
-import json
 
 app = Flask(__name__)
 
-
-def create_app():
-
-    @app.route('/get_all')
-    def get_all():
-        return "success"
-
-    return app
-
+@app.route('/')
+def index():
+	return "It's working"
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0')
+	app.run()
