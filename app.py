278a07fe7d44454176c3940c248258bc02ea048d
Kyle Bible <kyle.bible@gmail.com>
2017-10-30 16:54:19 -0700

    added a post route

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, request, redirect, send_from_directory
 
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.rout('/')
 def index():
-	return "It's working"
+    return "Nothing to see here"
+
+
+@app.route('/post_request', methods=['POST'])
+def poll_answer():
+    data = request.data.decode("utf-8")
+    return data
+
+@app.route('/', defaults={'path': ''})  # Catch All urls, enabling copy-paste url
+@app.route('/<path:path>')
+def home(path):
+    return redirect('/')
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
