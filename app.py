fd114dc920d842941180e717bb3bc4912dc0132c
Kyle Bible <kyle.bible@gmail.com>
2017-10-30 18:34:46 -0700

    fixed .json?

--- app.py
+++ app.py
@@ -3,21 +3,26 @@ from flask import Flask, request, redirect, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Nothing to see here"
+
+def create_app():
+    @app.route('/')
+    def index():
+        return "Nothing to see here"
 
 
-@app.route('/post_request', methods=['POST'])
-def poll_answer():
-    data = request.json
-    print(str(data))
-    return data
+    @app.route('/post_request', methods=['POST'])
+    def poll_answer():
+        data = request.json
+        print(data)
+        return "hi"
 
-@app.route('/', defaults={'path': ''})  # Catch All urls, enabling copy-paste url
-@app.route('/<path:path>')
-def home(path):
-    return redirect('/')
+    @app.route('/', defaults={'path': ''})  # Catch All urls, enabling copy-paste url
+    @app.route('/<path:path>')
+    def home(path):
+        return redirect('/')
+    
+    return app
 
 if __name__ == "__main__":
-    app.run()
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0')
