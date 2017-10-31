2d774939e5650e9f87f1506298da0d8512adc161
Kyle Bible <kyle.bible@gmail.com>
2017-10-30 18:55:28 -0700

    debugging

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 from flask import Flask, request, redirect, send_from_directory
 
-
 app = Flask(__name__)
 
 
-def create_app():
-    @app.route('/')
-    def index():
-        return "Nothing to see here"
+@app.route('/')
+def index():
+    return "Nothing to see here"
 
 
-    @app.route('/post_request', methods=['POST'])
-    def poll_answer():
-        # data = request.get_data()
-        # print("HERE'S THE DATA", data)
-        
-        return "hi"
+@app.route('/post_request', methods=['POST'])
+def poll_answer():
+    data = request.get_data()
+    print("HERE'S THE DATA", data)
+    return "hi"
 
-    @app.route('/', defaults={'path': ''})  # Catch All urls, enabling copy-paste url
-    @app.route('/<path:path>')
-    def home(path):
-        return redirect('/')
-    
-    return app
+@app.route('/', defaults={'path': ''})  # Catch All urls, enabling copy-paste url
+@app.route('/<path:path>')
+def home(path):
+    return redirect('/')
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
