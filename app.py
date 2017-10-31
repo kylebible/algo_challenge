ad0b89a712b38171ee38eba9be63c5198aa2b2f6
Kyle Bible <kyle.bible@gmail.com>
2017-10-30 19:07:34 -0700

    payload?

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/post_request', methods=['POST'])
 def poll_answer():
-    data = request.data
+    data = request.payload
     print("HERE'S THE DATA", data)
     return "hi"
 
