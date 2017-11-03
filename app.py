87f160c4e0e0047f00e105dfb61fc92f5c5c6e60
Kyle Bible <kyle.bible@gmail.com>
2017-11-03 07:49:48 -0700

    changed replace original back to false

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def results():
         return jsonify(message)
 
     message = {
-        "replace_original": "true",
+        "replace_original": "false",
         "text": "We've got your vote! Once the whole team's vote is in, I'll post the result!"
     }
 
